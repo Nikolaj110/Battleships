46c2dd82b8731c25fb19c1b50db58a28b9c15b71
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-03-03 23:38:34 +0100

    Testar runt lite med js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
-x = element.getAttribute("data-x");
-y = element.getAttrbiute("data-y");
+document.addEventListener('DOMContentLoaded', () => {
+    const ships = document.querySelector('.ship')
+})
+
+//x = element.getAttribute("data-x");
+//y = element.getAttrbiute("data-y");
 
 ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
 
 
+let selectedShipNameWithIndex
+let draggedShip
+let draggedShipLength
 
-function dragStart(e){
-    console.log(e.target)
-    console.log(this)
+console.log(draggedShip)
+
+ships.forEach(ship => ship.addEventListener('mousedown', (e) =>{
+    selectedShipNameWithIndex = e.target.id 
+    console.log(selectedShipNameWithIndex)
+}))
+
+
+function dragStart(){
+    draggedShip = this
+    draggedShipLength = draggedShip.length
 }
