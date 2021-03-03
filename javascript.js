b13c48eb587bb58f2ee9d88830ab41cde15bf59a
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-03-03 21:15:20 +0100

    Börjar med js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
 x = element.getAttribute("data-x");
 y = element.getAttrbiute("data-y");
+
+ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+
+
+
+function dragStart(e){
+    console.log(e.target)
+    console.log(this)
+}
