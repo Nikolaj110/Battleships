2cd0cb33fe897a5ef16d36f0b3bb039fe18b3690
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-03-06 13:23:33 +0100

    Datorns slumpvisade skepp

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
+/*
 
+//x = element.getAttribute("data-x");
+//y = element.getAttrbiute("data-y");
 
 document.addEventListener('DOMContentLoaded', () => {
     const ships = document.querySelectorAll('.ship')
@@ -7,42 +10,73 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-    //x = element.getAttribute("data-x");
-//y = element.getAttrbiute("data-y");
+    
 
-ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
-ruta => ruta.addEventListener('dragover', dragOver)
-ruta => ruta.addEventListener('dragenter', dragEnter)
-ruta => ruta.addEventListener('dragleave', dragLeave)
-ruta => ruta.addEventListener('drop', drarDrop)
-ruta => ruta.addEventListener('dragend', dragEnd)
-
+ ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+ ruta => ruta.addEventListener('dragover', dragOver)
+ ruta => ruta.addEventListener('dragenter', dragEnter)
+ ruta => ruta.addEventListener('dragleave', dragLeave)
+ ruta => ruta.addEventListener('drop', dragDrop)
+ ruta => ruta.addEventListener('dragend', dragEnd)
 
 
 
 
-let selectedShipNameWithIndex
-let draggedShip
-let draggedShipLength
+
+ let selectedShipNameWithIndex
+ let draggedShip
+ let draggedShipLength
 
 
-ships.forEach(ship => ship.addEventListener('mousedown', (e) =>{
+ ships.forEach(ship => ship.addEventListener('mousedown', (e) =>{
     selectedShipNameWithIndex = e.target.id 
     console.log(selectedShipNameWithIndex)
-}))
+ }))
 
 
-function dragStart(){
+ function dragStart(){
     draggedShip = this
     draggedShipLength = draggedShip.length
     console.log(draggedShip)
-}
+    console.log(draggedShipLength)
+ }
+
+ function dragOver(e){
+    e.preventDefault()
+ }
+
+ function dragEnter(e){
+    e.preventDefault()
+ }
+
+ function dragLeave(){
+    console.log('drag leave')
+ }
+
+ function dragDrop(){
+    let shipNameWithLastId = draggedShip.lastChild.id
+    let shipClass = shipNameWithLastId.slice(0,-2)
+    console.log(shipClass)
+ }
+
+ function dragEnd(){
+     
+ }
+
+
+
+
+
+
 })
 
 
 
 
-/*
+
+
+
+*/
 
 document.addEventListener('DOMContentLoaded', () =>{
     const userGrid = document.querySelector('.grid-user')
@@ -67,22 +101,85 @@ document.addEventListener('DOMContentLoaded', () =>{
     function createBoard(grid, squares){
     for (let i = 0; i < width*width; i++){
         const square = document.createElement('div')
-        squares.dataset.id = i
+        square.dataset.id = i
         grid.appendChild(square)
         squares.push(square) 
     }
-}
+ }
 
-createBoard(userGrid, userSquares)
-createBoard(computerGrid, computerSquares)
+ createBoard(userGrid, userSquares)
+ createBoard(computerGrid, computerSquares)
 
+
+
+ const shipArray = [
+    {
+         name: 'destroyer',
+         directions:[
+             [0, 1]
+             [0, width]
+         ]
+    },
+    {
+         name: 'submarine',
+         directions:[
+             [0, 1, 2]
+             [0, width, width*2]
+         ]
+    },
+    {
+         name:'cruiser',
+         directions:[
+             [0, 1, 2]
+             [0, width, width*2]
+         ]
+    },
+    {
+         name:'battleship',
+         directions:[
+             [0, 1, 2, 3]
+             [0, width, width*2, width*3]
+         ]
+    },
+    {
+         name:'carrier',
+         directions:[
+             [0, 1, 2, 3, 4]
+             [0, width, width*2, width*3, width*4]
+         ]
+    },
+    
+ ]
+
+
+ function generate(ship){
+ let randomDirection = Math.floor(Math.random() * ship.directions.length)
+ let current = ship.directions[randomDirection]
+ if(randomDirection === 0) direction = 1
+ if(randomDirection === 1) direction = 10
+ console.log(directions[0])
+ let x = ship.directions[0].length
+ let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction))
+
+ const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+ const isAtRigthEdge = current.some(index => (randomStart + index) % width === width - 1)
+ const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+ console.log(isTaken)
+
+ if(!isTaken && !isAtRigthEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+
+ else generate(ship)
+ }
+
+ generate(shipArray[0])
+ generate(shipArray[1])
 
 
 
 
 })
 
-*/
+
 
 
 
