c1aa8a48ec101a959fd1458bc2a9aaf4716d8457
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-03-06 01:20:48 +0100

    Fixa de två rutnäten

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
+
+/*
 document.addEventListener('DOMContentLoaded', () => {
-    const ships = document.querySelector('.ship')
-    const rutor = document.querySelector('ruta')
+    const ships = document.querySelectorAll('.ship')
+    const rutor = document.querySelectorAll('.ruta')
 })
 
 
@@ -8,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //y = element.getAttrbiute("data-y");
 
 ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+rutor.forEach(ruta => ruta.addEventListener('dragstart', dragStart))
 
 
 let selectedShipNameWithIndex
@@ -26,3 +29,40 @@ function dragStart(){
     draggedShipLength = draggedShip.length
     console.log(draggedShip)
 }
+
+*/
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const displayGrid = document.querySelector('grid-display')
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
+    const startButton = document.querySelector('#start')
+    const rotateButton = document.querySelector('#rotate')
+    const turnDisplay = document.querySelector('#whose-go')
+    const infoDisplay = document.querySelector('#info')
+    const userSquares = []
+    const computerSquares = []
+
+    const width = 10
+})
+
+
+function createBoard(grid, squares){
+    for (let i = 0; i < width*width; i++){
+        const square = document.createElement('div')
+        squares.dataset.id = i
+        grid.appendChild(square)
+        squares.push(square) 
+    }
+}
+
+createBoard(userGrid, userSquares)
+createBoard(computerGrid, computerSquares)
+
+
